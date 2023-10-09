24d414987333a7e815a50904c6711bf3c97c2537
fchelotti <fchelotti@gmail.com>
2023-10-08 21:15:42 -0300

    exercicios nextjs11

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+import './css/integracao1.css'
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,20 @@
+export default function jsx1Diferente () {
+    const array = [
+        <li>Paulo</li>,
+        <li>Ana</li>,
+        <li>Pedro</li>,
+    ]
+    
+    //return array
+
+    const conteudo = (
+        <div>
+            <h1 className="vermelha">JSX #02</h1>
+        </div>
+    )
+    return conteudo
+}
+
+// HTML, CSS e JS - Tecnologias
+
+// Componentes ()
